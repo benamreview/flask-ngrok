84babcfc5e6cc4c0e9f07bb4b956326acda6a77d
Duy Ho <dhh3hb@mail.umkc.edu>
2021-01-09 03:08:38 -0600

    Update flask_ngrok.py

--- flask_ngrok.py
+++ flask_ngrok.py
@@ -20,7 +20,18 @@ def _get_command():
     elif system == "Windows":
         command = "ngrok.exe"
     elif system == "Linux":
-        command = "ngrok"
+        command = "http -hostname=nsfscc.ngrok.io 80"
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)  # Start ngrok when app is run
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()"
     else:
         raise Exception("{system} is not supported".format(system=system))
     return command
